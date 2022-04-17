5241879d55dbb3578289c42d349c4f3357b5f5ae
peterlin0701 <peterlin0701@sumire.com>
2022-04-17 15:56:08 +0800

    *

---
+++ ASK.TestDrivenDevelopment/ROCDateTimeExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASK.TestDrivenDevelopment
+{
+    public static class ROCDateTimeExtensions
+    {
+
+        public static string UtcDate(this DateTime target)
+        {
+            CultureInfo culture = new CultureInfo("zh-TW");
+            culture.DateTimeFormat.Calendar = new TaiwanCalendar();
+            var result = target.ToLocalTime().ToString("yyy.MM.dd", culture);
+
+            return result;
+        }
+
+
+        public static string LittleBitDate(this DateTime target)
+        {
+            CultureInfo culture = new CultureInfo("zh-TW");
+            culture.DateTimeFormat.Calendar = new TaiwanCalendar();
+            var result = target.ToString("yyy.MM.dd", culture);
+            return result;
+        }
+
+
+        public static string ShortDate(this DateTime target)
+        {
+            CultureInfo culture = new CultureInfo("zh-TW");
+            culture.DateTimeFormat.Calendar = new TaiwanCalendar();
+            var result = target.ToLocalTime().ToString("yyy年MM月dd日", culture);
+
+            return result;
+        }
+
+
+        public static string LognData(this DateTime target)
+        {
+            CultureInfo culture = new CultureInfo("zh-TW");
+            culture.DateTimeFormat.Calendar = new TaiwanCalendar();
+            var result = target.ToLocalTime().ToString("民國yyy年MM月dd日", culture);
+
+            return result;
+        }
+
+
+        public static string LongDateAndWeek(this DateTime target)
+        {
+            CultureInfo culture = new CultureInfo("zh-TW");
+            culture.DateTimeFormat.Calendar = new TaiwanCalendar();
+            var result = target.ToLocalTime().ToString("民國yyy年MM月dd日 週三", culture);
+
+            return result;
+        }
+
+
+
+        public static string LongDateAndLongWeek(this DateTime target)
+        {
+            CultureInfo culture = new CultureInfo("zh-TW");
+            culture.DateTimeFormat.Calendar = new TaiwanCalendar();
+            var result = target.ToLocalTime().ToString("民國yyy年MM月dd日 星期三", culture);
+
+            return result;
+        }
+    }
+}
